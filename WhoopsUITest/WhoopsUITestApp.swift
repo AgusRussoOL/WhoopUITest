a5dd9c2b0294f9ce9be3831200843fdcc2b257ab
Agustin Russo <agustin.russo@hotmail.com>
2024-02-27 15:48:38 -0300

    Initial Commit

---
+++ WhoopsUITest/WhoopsUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoopsUITestApp.swift
+//  WhoopsUITest
+//
+//  Created by Agustin Russo on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhoopsUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
